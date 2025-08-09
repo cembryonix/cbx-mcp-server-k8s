19c78fbbc662618390bb4772315b49b0c68093d1
vkuusk <vkuusk@gmail.com>
2025-08-09 10:43:12 -0700

    - update minor version

--- test/start-docker.sh
+++ test/start-docker.sh
@@ -7,7 +7,7 @@ docker run -it \
   -p 8080:8080 \
   -v ~/.kube:/home/appuser/.kube:ro \
   -v ~/.config/argocd:/home/appuser/.config/argocd:ro \
-  cbx-mcp-server-k8s:develop
+  ghcr.io/cembryonix/cbx-mcp-server-k8s:v0.1.1
 
 #  ghcr.io/cembryonix/cbx-mcp-server-k8s:v0.1.0
 # cbx-mcp-server-k8s:develop
